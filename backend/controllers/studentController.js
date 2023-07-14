6f817f745333563d8739f46bdc1494e626083c29
DErpewgyt <derekchia77@gmail.com>
2023-07-14 17:12:31 +0800

    Implement login functions & JWT Token #4

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -44,31 +44,4 @@ module.exports.deleteData = function (req, res, next) {
 };
 
 
-// module.exports.deleteData = function (req, res, next) {
-
-//     console.log("req.body", req.body);
-//     const sql = `DROP TABLE IF EXISTS spoc.Test_Student;
-
-//     CREATE TABLE spoc.Test_Student (
-//         PKNo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-//         AdminNo VARCHAR(99) NOT NULL DEFAULT '0',
-//         StudentName VARCHAR(99) DEFAULT NULL,
-//         AttemptKeratometer INT NOT NULL DEFAULT '0',
-//         CompletedKeratometer INT NOT NULL DEFAULT '0',
-//         AttemptedPhoropter INT NOT NULL DEFAULT '0',
-//         CompletedPhoropter INT NOT NULL DEFAULT '0'
-//     );
-//     `;
-//     const values = [req.body.PKNo];
-//     connection.promise().query(sql, values)
-//         .then(([rows, fields]) => {
-//             console.log(rows);
-//             res.json(rows);
-//         })
-//         .catch((error) => {
-//             res.send(error);
-//         });
-// }
-
-
 
