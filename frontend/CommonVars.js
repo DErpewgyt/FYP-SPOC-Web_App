2e066f272b64ea85fa5fbb34b75aba5cbb7cf3d5
DErpewgyt <derekchia77@gmail.com>
2023-07-18 15:31:04 +0800

    test

--- frontend/CommonVars.js
+++ frontend/CommonVars.js
@@ -1,10 +1,10 @@
-// let API_URL = (url) => {
-//     return ((url.includes('localhost') || url.includes('127.0.0.1')) != true ? 'https://derpe.azurewebsites.net' : 'http://localhost:3000');
-// }
-
-
-function isLocalhost(url) {
-    return url.includes('localhost') || url.includes('127.0.0.1');
+let API_URL = (url) => {
+    return ((url.includes('localhost') || url.includes('127.0.0.1')) != true ? 'https://spoc-simulation.onrender.com/' : 'http://localhost:3000');
 }
 
-API_URL = (isLocalhost(location.hostname) != true ? 'https://' + location.hostname : 'http://localhost:3000');
+
+// function isLocalhost(url) {
+//     return url.includes('localhost') || url.includes('127.0.0.1');
+// }
+
+// API_URL = (isLocalhost(location.hostname) != true ? 'https://' + location.hostname : 'http://localhost:3000');
