8dea36b5a98bf37e7614bbd6cb400009a532b695
DErpewgyt <derekchia77@gmail.com>
2023-07-18 15:25:44 +0800

    test

--- backend/db.js
+++ backend/db.js
@@ -5,24 +5,24 @@
 /* Include to use .env file */
 require('dotenv').config();
 
-const mysql = require('mysql2/promise')
+const mysql = require('mysql2')
 
 //////////////////////////////////////////////////////////////////////////
 // DATABASE_URL extracted from .env file
 //////////////////////////////////////////////////////////////////////////
 
-//const connection = mysql.createConnection(process.env.DATABASE_URL)
+const connection = mysql.createConnection(process.env.DATABASE_URL)
 
-const connection = mysql.createConnection({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    connectionLimit: process.env.DB_CONNECTION_LIMIT,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-});
+// const connection = mysql.createConnection({
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_DATABASE,
+//     connectionLimit: process.env.DB_CONNECTION_LIMIT,
+//     ssl: {
+//         rejectUnauthorized: false,
+//     },
+// });
 console.log("DB Connected");
 
 module.exports = connection;
