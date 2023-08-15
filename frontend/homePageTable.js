dc92680959b15d05f46abfd5d9efe796e6bc9333
PewPewPoopity <jeremyneo2002@hotmail.com>
2023-08-15 10:35:21 +0800

    Minor Fixes

--- frontend/homePageTable.js
+++ frontend/homePageTable.js
@@ -1,19 +1,22 @@
+function convertToSingaporeTime(utcTimeString) {
+    const options = {
+        year: 'numeric', month: '2-digit', day: '2-digit',
+        hour: '2-digit', minute: '2-digit', second: '2-digit',
+        timeZone: 'Asia/Singapore' // Set to the appropriate time zone
+    };
+
+    const utcTime = new Date(utcTimeString);
+    return utcTime.toLocaleString(undefined, options);
+}
+
 function isLocalhost(url) {
     return url.includes('localhost') || url.includes('127.0.0.1');
 }
-//var window = this.window;
 
 API_URL = (isLocalhost(location.hostname) != true ? 'https://' + location.hostname : 'http://localhost:3000');
 
-function addToTable(AdminNo, StudentName, AttemptKera, AttemptPhoro, CompleteKera, CompletePhoro,FirstAttempt,LastAttempt) {
+function addToTable(AdminNo, StudentName, AttemptKera, AttemptPhoro, CompleteKera, CompletePhoro, FirstAttempt, LastAttempt) {
     const template = document.querySelector('#row-template');
-    // var templateid = document.getElementById('row-template');
-    // var tablerow = templateid.getElementsByTagName('tr')
-
-    // var img = document.getElementById('image')
-    // const img = document.getElementById("image")
-    // row = document.getElementById('row-template')
-
     const row = template.content.firstElementChild.cloneNode(true);
     row.querySelector('.AdminNo').textContent = AdminNo;
     row.querySelector('.StudentName').textContent = StudentName;
@@ -21,18 +24,11 @@ function addToTable(AdminNo, StudentName, AttemptKera, AttemptPhoro, CompleteKer
     row.querySelector('.AttemptPhoro').textContent = AttemptPhoro;
     row.querySelector('.CompleteKera').textContent = CompleteKera;
     row.querySelector('.CompletePhoro').textContent = CompletePhoro;
-    row.querySelector('.FirstAttempt').textContent = FirstAttempt;
-    row.querySelector('.LastAttempt').textContent = LastAttempt;
-
-     
-     const firstAttemptTime = FirstAttempt ? new Date(FirstAttempt) : null;
-     const lastAttemptTime = LastAttempt ? new Date(LastAttempt) : null;
- 
-    
-     const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' };
-     row.querySelector('.FirstAttempt').textContent = firstAttemptTime ? firstAttemptTime.toLocaleString(undefined, options) : 'N/A';
-     row.querySelector('.LastAttempt').textContent = lastAttemptTime ? lastAttemptTime.toLocaleString(undefined, options) : 'N/A';
- 
+    row.querySelector('.FirstAttempt').textContent = convertToSingaporeTime(FirstAttempt);
+    row.querySelector('.LastAttempt').textContent = convertToSingaporeTime(LastAttempt);
 
     document.querySelector('#module-tbody').appendChild(row);
 }
+
+// Call addToTable with your data
+// addToTable(AdminNo, StudentName, AttemptKera, AttemptPhoro, CompleteKera, CompletePhoro, FirstAttempt, LastAttempt);
